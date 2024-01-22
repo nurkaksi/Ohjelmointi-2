a8c379f7906a55371ded5b117093341948d77949
Sara Nurminen <sara.nurminen@tuni.fi>
2024-01-22 10:40:07 +0200

    palautus lämpötila 2

--- student/02/temperature/main.cpp
+++ student/02/temperature/main.cpp
@@ -3,10 +3,13 @@
 using namespace std;
 
 int main()
-{
+{   double temperature = 0;
     cout << "Enter a temperature: ";
+    cin >> temperature;
 
-    // Write your code here
+    cout << temperature << " degrees Celcius is " << (temperature * 9 / 5) + 32 << " degrees Fahrenheit" << endl;
+    cout << temperature << " dehrees Fahrenheit is " << (temperature - 32)* 5 / 9 << " degrees Celcius" << endl;
+
 
     return 0;
 }
