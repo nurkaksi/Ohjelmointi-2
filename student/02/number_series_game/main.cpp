86d8ac894810a9aa39c64b9e65744a0d05232e35
Sara Nurminen <sara.nurminen@tuni.fi>
2024-01-22 11:07:29 +0200

    zip boing 1

--- student/02/number_series_game/main.cpp
+++ student/02/number_series_game/main.cpp
@@ -1,11 +1,29 @@
 #include <iostream>
-
+using namespace std;
 
 int main()
 {
+    int luku = 0;
+    int counter = 1;
     std::cout << "How many numbers would you like to have? ";
+    cin >> luku;
+    while (counter <= luku) {
+        if (counter % 3 == 0 and counter % 7 == 0) {
+            cout << "zip boing";
+        }
+        else if (counter % 3 ==0) {
+            cout << "zip";
+        }
+        else if (counter % 7 ==0) {
+            cout << "boing";
+        }
+        else {
+            cout << counter;
+        }
+        cout << endl;
+        ++counter;
+    }
 
-    // Write your code here
 
     return 0;
 }
