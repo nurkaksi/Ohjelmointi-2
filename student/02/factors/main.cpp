3a598e6ab0a5d7b05ff57488b9632512a4c62daf
Sara Nurminen <sara.nurminen@tuni.fi>
2024-01-24 19:08:00 +0200

    tekijät

--- student/02/factors/main.cpp
+++ student/02/factors/main.cpp
@@ -1,12 +1,31 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
+void finding_numbers(int number) {
+    int i = 1;
+    int f1 = 0;
+    int f2 = 0;
+    for ( i = 1; i <= sqrt(number); ++i) {
+        if ( number % i == 0 ) {
+            f1 = i;
+            f2 = number / i;
+        }
+    }
+    cout << number << " = " << f1 << " * " << f2 << endl;
+}
+
 int main()
 {
+    int number = 0;
     cout << "Enter a positive number: ";
-
-    // Write your code here
+    cin >> number;
+    if ( number <= 0) {
+        cout << "Only positive numbers accepted" << endl;
+    } else {
+        finding_numbers(number);
+    }
 
     return 0;
 }
