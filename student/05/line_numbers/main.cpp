2d225af344f88058031a590108afedebb0491c7a
Sara Nurminen <sara.nurminen@tuni.fi>
2024-02-12 19:19:53 +0200

    linenumbers

---
+++ student/05/line_numbers/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string InputFileName = "";
+    string OutputFileName = "";
+    cout << "Input file: ";
+    getline(cin, InputFileName);
+    cout << "Output file: ";
+    getline(cin, OutputFileName);
+
+    ifstream InputFile(InputFileName);
+    if ( not InputFile ) {
+            cout << "Error! The file "<< InputFileName <<" cannot be opened." << endl;
+        }
+    else {
+        ofstream outputFile(OutputFileName);
+
+        int LineNumber = 1;
+        string line;
+
+        while (getline(InputFile, line)) {
+            // Kirjoitetaan rivi tulostiedostoon rivinumeron kera
+            outputFile << LineNumber << " " << line << endl;
+            LineNumber++;
+        }
+        InputFile.close();
+        outputFile.close();
+
+    }
+
+
+
+    return 0;
+}
