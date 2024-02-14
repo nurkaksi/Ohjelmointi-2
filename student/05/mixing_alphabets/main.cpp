ec0dc8fc27797e7ed316f0037ffdf769df4e7f70
Sara Nurminen <sara.nurminen@tuni.fi>
2024-02-14 17:33:50 +0200

    kirjaimet

---
+++ student/05/mixing_alphabets/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <random>
+
+int main()
+{
+    // This is a random number generator that should be given as parameter to the
+    // function of the algorithm library to shuffle letters
+    std::minstd_rand generator;
+
+    std::cout << "Enter some text. Quit by entering the word \"END\"." << std::endl;
+    std::string word;
+
+    while (std::cin >> word)
+    {
+        if (word == "END")
+        {
+            return EXIT_SUCCESS;
+        }
+        else {
+            if ( word.size() > 2 ) {
+                        std::string::iterator first = word.begin();
+                        std::string::iterator last = word.end();
+                        ++first;
+                        --last;
+
+                        shuffle(first, last, generator);
+                        }
+        }
+           std::cout << word << std::endl;
+    }
+    return EXIT_SUCCESS;
+}
