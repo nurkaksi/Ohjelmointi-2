92d13f28adb78f9030cf180f1b2d1127bdc794fe
Sara Nurminen <sara.nurminen@tuni.fi>
2024-04-05 19:19:49 +0300

    careperiod.hh tehty

--- student/10/hospital/careperiod.hh
+++ student/10/hospital/careperiod.hh
@@ -35,14 +35,61 @@ public:
       */
     ~CarePeriod();
 
-    // More public methods
+    /**
+     * @brief add_staff
+     * @param staff_id
+     * @param staff
+     * Adds staff to careperiod
+     */
+    void add_staff(std::string staff_id, Person* staff);
+
+    // is_staff(Person* staff) onko joku tietty henkilö työskennellyt täällä
+
+    /**
+     * @brief set_end_date
+     * @param end
+     * Set an end date to careperiod when patient leaves
+     */
+    void set_end_date(const Date& end);
+
+    /**
+     * @brief get_patient
+     * @return patient
+     */
+    Person *get_patient();
+
+    /**
+     * @brief get_care_period_start_time
+     * @return care period start time
+     */
+    Date get_care_period_start_time();
+
+    /**
+     * @brief get_care_period_end_time
+     * @return care period ending time
+     */
+    Date get_care_period_end_time();
+
+    /**
+     * @brief get_staff_
+     * @return patients staff
+     */
+    std::map<std::string, Person*> get_staff_();
+
+    /**
+     * @brief print_dates
+     * Prints care periods dates
+     */
+    void print_dates();
+
 
 private:
     Person* patient_;
     Date start_;
     Date end_;
 
-    // More attributes and methods
+    std::map<std::string, Person*> staff_;
+
 };
 
 #endif // CAREPERIOD_HH
